aeac6442bf1f848c682cb289b37dc075313d3c08
andressilva182 <andressilva182@gmail.com>
2024-03-02 16:29:35 -0800

    git setup

--- AA_Project/Intermediate/Build/Win64/x64/AA_ProjectEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++ AA_Project/Intermediate/Build/Win64/x64/AA_ProjectEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "C:/Users/Andres PC/Documents/Unreal Projects/AA_Project/Intermediate/Build/Win64/x64/AA_ProjectEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
+#include "C:/Users/Andres PC/Documents/Unreal Projects/Respositories/AA/AA-Project/AA_Project/Intermediate/Build/Win64/x64/AA_ProjectEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
 #include "Runtime/Slate/Public/SlateSharedPCH.h"
